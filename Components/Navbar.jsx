2eeb243014cda25c575592188e582e26252db499
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-02-18 23:44:46 +0530

    fix

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -10,11 +10,11 @@ export default function Navbar() {
                 <img src="/Assets/LogoBlack.png" alt="" />
             </div>
             <ul className={`navbarElements ${sideBar? '' : 'sideBarHide'}`}>
-                <li className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/"><a>HOME</a></Link></li>
-                <li className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/portfolio"><a>PORTFOLIO</a></Link></li>
-                <li className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/services"><a>SERVICES</a></Link></li>
-                {/* <li className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/about-us"><a>ABOUT US</a></Link></li> */}
-                <li className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/contact-us"><a>CONTACT US</a></Link></li>
+                <li onClick = {() => {setToggle(false); setSideBar(false)}} className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/" ><a>HOME</a></Link></li>
+                <li onClick = {() => {setToggle(false); setSideBar(false)}} className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/portfolio"><a>PORTFOLIO</a></Link></li>
+                <li onClick = {() => {setToggle(false); setSideBar(false)}} className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/services"><a>SERVICES</a></Link></li>
+                {/* <li onClick = {() => {setToggle(false); setSideBar(false)}} className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/about-us"><a>ABOUT US</a></Link></li> */}
+                <li onClick = {() => {setToggle(false); setSideBar(false)}} className={`navItem ${sideBar? '' : 'navItemSlide'}`}><Link href="/contact-us"><a>CONTACT US</a></Link></li>
             </ul>
             <div className='sideDrawer'>
                 <button className='toggleButton' onClick = {() => {setToggle(!toggle); setSideBar(!sideBar)}}>
