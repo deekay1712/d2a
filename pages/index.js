95f27617856c505ed749ecdf04f64b7938d69f59
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2022-03-02 20:48:00 +0530

    meta tags added to home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import CarouselHome from "../Components/CarouselHome";
 import HomeAbout from "../Components/HomeAbout";
 import HomeCards from "../Components/HomeCards";
 import HomeWhyUs from "../Components/HomeWhyUs";
+import Head from 'next/head'
 
 export default function Home() {
   const homeCarouselData = [
@@ -23,6 +24,23 @@ export default function Home() {
   ];
   return (
     <>
+      <Head>
+        <title>HOME - Dream Design Architects</title>
+        <meta name="description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <link rel="canonical" href="https://dreamdesignarchitects.com/" />
+        <meta name="googlebot" content="notranslate" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="HOME - Dream Design Architects" />
+        <meta property="og:description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta property="og:url" content="https://dreamdesignarchitects.com/" />
+        <meta property="og:site_name" content="Dream Design Architects" />
+        <meta property="og:image" content="/Assets/carousel1.jpeg" />
+        <meta property="og:image:secure_url" content="/Assets/carousel1.jpeg" />
+        <meta name="twitter:description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta name="twitter:title" content="HOME - Dream Design Architects" />
+        <meta name="twitter:image" content="/Assets/carousel1.jpeg" />
+      </Head>
       <CarouselHome homeCarouselData={homeCarouselData} />
       <HomeAbout />
       <HomeCards />
