d537ce6158718a9b1ff88fae08412af66301ada8
DeeKay <dikshansh1712@gmail.com>
2022-03-13 12:01:11 +0530

    prevent load after submit in contact form

--- Components/ContactUs.jsx
+++ Components/ContactUs.jsx
@@ -11,7 +11,7 @@ export default function ContactUs() {
         }, (error) => {
             console.log(error.text);
       });
-      window.location.reload();
+    //   window.location.reload();
     }
   return (
     <div className="contactUsWrapper">
