0dd5eceb4d47f7dcc18e780e19b6d8aa8850b2ac
DeeKay <dikshansh1712@gmail.com>
2022-06-18 23:16:43 +0530

    service page updated

--- Components/ServicesComp.jsx
+++ Components/ServicesComp.jsx
@@ -34,8 +34,18 @@ export default function ServicesComp() {
             </div>
             <div className="servicePartitionDiv"></div>
             <div className="serviceText">
-              <h2 className="serviceTextHeadingL">Turnkey Projects</h2>
-              <p className="serviceTextContentL">D2A offers consultancy and project management services on turnkey projects basis in which it provides a wide range of services including planning, procurement and furnishing of building along with other necessary services.</p>
+              <h2 className="serviceTextHeadingL">2D & 3D Drafting & Modeling</h2>
+              <p className="serviceTextContentL">The D2A delivers wide-ranging professional, Quality and Cost efficient timely delivery 2D Drafting services to Architects, Interior Designers, General Contractors and Woodworkers. <br/>We are a team of talented and professional Architects & Interior Designers to provide the best services with tight deadlines without compromising on the drawings quality. Our technical team utilizes AutoCAD (2D) and Sketchup (3D) programmes to maintain accuracy in drawings and providing 4 rounds of Markups/Readlines.</p>
+            </div>
+          </div>
+          <div className="serviceR">
+            <div className="serviceText">
+              <h2 className="serviceTextHeadingR">Turnkey Projects</h2>
+              <p className="serviceTextContentR">D2A offers consultancy and project management services on turnkey projects basis in which it provides a wide range of services including planning, procurement and furnishing of building along with other necessary services.</p>
+            </div>
+            <div className="servicePartitionDiv"></div>
+            <div className="serviceImgR">
+              <img src="/Assets/service4.jpeg" alt="" />
             </div>
           </div>
         </div>
