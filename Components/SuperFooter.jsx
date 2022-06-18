6b591dba0e21e25bdbf96e520872abd7e5a1058e
DeeKay <dikshansh1712@gmail.com>
2022-06-18 23:18:50 +0530

    linkedin page URL update

--- Components/SuperFooter.jsx
+++ Components/SuperFooter.jsx
@@ -5,7 +5,7 @@ export default function SuperFooter() {
     <div className="superFooterWrapper">
         <p className="superFooterWrapperText">Copyright © 2022 Dream Design Architects - Developed by <a href="https://www.linkedin.com/in/deekay1712/">deekay</a></p>
         <div className="superFooterSocialIcons">
-            <a href="https://www.linkedin.com/in/dream-design-architects-800145231/"><img className="socialMediaIcons" src="/Assets/linkedin.png" alt="" /></a>
+            <a href="https://www.linkedin.com/company/dream-design-architects-d2a/"><img className="socialMediaIcons" src="/Assets/linkedin.png" alt="" /></a>
             <a href="https://www.facebook.com/Dream-Design-Architects-106902275278119/"><img className="socialMediaIcons" src="/Assets/facebook.png" alt="" /></a>
             <a href="https://www.instagram.com/d_two_a/"><img className="socialMediaIcons" src="/Assets/instagram.png" alt="" /></a>
         </div>
