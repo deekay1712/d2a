0714a70efa7098616e0a8694951946dff47cb298
Dikshansh Kumar Lodhi <80641727+deekay1712@users.noreply.github.com>
2022-12-06 21:08:44 +0530

    Update SuperFooter.jsx

--- Components/SuperFooter.jsx
+++ Components/SuperFooter.jsx
@@ -3,7 +3,7 @@
 export default function SuperFooter() {
   return (
     <div className="superFooterWrapper">
-        <p className="superFooterWrapperText">Copyright © 2022 Dream Design Architects - Developed by <a href="https://www.linkedin.com/in/deekay1712/">deekay</a></p>
+        <p className="superFooterWrapperText">Copyright © 2022 D2A Atelier - Developed by <a href="https://www.linkedin.com/in/deekay1712/">deekay</a></p>
         <div className="superFooterSocialIcons">
             <a href="https://www.linkedin.com/company/dream-design-architects-d2a/"><img className="socialMediaIcons" src="/Assets/linkedin.png" alt="" /></a>
             <a href="https://www.facebook.com/Dream-Design-Architects-106902275278119/"><img className="socialMediaIcons" src="/Assets/facebook.png" alt="" /></a>
