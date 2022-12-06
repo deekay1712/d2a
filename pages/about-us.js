adb9cc5f5694a11a24aed734e5be36542e4311de
Dikshansh Kumar Lodhi <80641727+deekay1712@users.noreply.github.com>
2022-12-06 21:13:44 +0530

    Update about-us.js

--- pages/about-us.js
+++ pages/about-us.js
@@ -13,19 +13,19 @@ export default function aboutUs() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta name="description" content="D2A Atelier is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
         <link rel="canonical" href="https://dreamdesignarchitects.com/about-us/" />
         <meta name="googlebot" content="notranslate" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="SERVICES - Dream Design Architects" />
+        <meta property="og:title" content="SERVICES - D2A Atelier" />
         <meta property="og:description" content="We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
-        <meta property="og:url" content="https://dreamdesignarchitects.com/about-us/" />
-        <meta property="og:site_name" content="Dream Design Architects" />
+        <meta property="og:url" content="https://www.d2aatelier.com/" />
+        <meta property="og:site_name" content="D2A Atelier" />
         <meta property="og:image" content="/Assets/carousel1.jpeg" />
         <meta property="og:image:secure_url" content="/Assets/carousel1.jpeg" />
-        <meta name="twitter:description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
-        <meta name="twitter:title" content="ABOUT US - Dream Design Architects" />
+        <meta name="twitter:description" content="D2A Atelier is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta name="twitter:title" content="ABOUT US - D2A Atelier" />
         <meta name="twitter:image" content="/Assets/carousel1.jpeg" />
       </Head>
       <AboutUs/>
