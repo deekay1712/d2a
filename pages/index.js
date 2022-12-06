58873f3430daf53752ff7845f05fb0fb89b60677
Dikshansh Kumar Lodhi <80641727+deekay1712@users.noreply.github.com>
2022-12-06 21:18:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>HOME - Dream Design Architects</title>
+        <title>HOME - D2A Atelier</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
