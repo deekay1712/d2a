7dd12243495f9c3f434bf01c17a7dc9a0b3b963c
Dikshansh Kumar Lodhi <80641727+deekay1712@users.noreply.github.com>
2022-12-06 21:12:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,19 +48,19 @@ export default function Home() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta name="description" content="D2A Atelier is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
         <link rel="canonical" href="https://dreamdesignarchitects.com/" />
         <meta name="googlebot" content="notranslate" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="HOME - Dream Design Architects" />
+        <meta property="og:title" content="HOME - D2A Atelier" />
         <meta property="og:description" content="We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
         <meta property="og:url" content="https://dreamdesignarchitects.com/" />
-        <meta property="og:site_name" content="Dream Design Architects" />
+        <meta property="og:site_name" content="D2A Atelier" />
         <meta property="og:image" content="/Assets/carousel1.jpeg" />
         <meta property="og:image:secure_url" content="/Assets/carousel1.jpeg" />
-        <meta name="twitter:description" content="Dream Design Architects is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
-        <meta name="twitter:title" content="HOME - Dream Design Architects" />
+        <meta name="twitter:description" content="D2A Atelier is a design firm based in the heart of the city of DELHI. We are a team of creative and experienced professionals who are passionate about creating beautiful spaces." />
+        <meta name="twitter:title" content="HOME - D2A Atelier" />
         <meta name="twitter:image" content="/Assets/carousel1.jpeg" />
       </Head>
       <CarouselHome homeCarouselData={homeCarouselData} />
