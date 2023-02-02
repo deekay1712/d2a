60c7afabc00ea9c73456af8c2fb23ca1b3bb315a
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-02-03 00:07:15 +0530

    :hammer: super footer edit

--- Components/SuperFooter.jsx
+++ Components/SuperFooter.jsx
@@ -3,12 +3,7 @@
 export default function SuperFooter() {
   return (
     <div className="superFooterWrapper">
-        <p className="superFooterWrapperText">Copyright © 2022 D2A Atelier - Developed by <a href="https://www.linkedin.com/in/deekay1712/">deekay</a></p>
-        <div className="superFooterSocialIcons">
-            <a href="https://www.linkedin.com/company/dream-design-architects-d2a/"><img className="socialMediaIcons" src="/Assets/linkedin.png" alt="" /></a>
-            <a href="https://www.facebook.com/Dream-Design-Architects-106902275278119/"><img className="socialMediaIcons" src="/Assets/facebook.png" alt="" /></a>
-            <a href="http://instagram.com/d2a_atelier"><img className="socialMediaIcons" src="/Assets/instagram.png" alt="" /></a>
-        </div>
+        <p className="superFooterWrapperText">Copyright © 2023 D2A Atelier <span className="developer">- Developed by <a href="https://www.linkedin.com/in/deekay1712/">deekay</a></span></p>
     </div>
   )
 }
