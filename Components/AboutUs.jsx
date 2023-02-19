2400744d2b82dd3bf4e5932ff4f2a8d7ac2446e8
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-02-19 23:26:11 +0530

    :hammer: add team section & update about section

--- Components/AboutUs.jsx
+++ Components/AboutUs.jsx
@@ -1,38 +1,42 @@
+import Team from "./Team"
 export default function AboutUs() {
   return (
     <div className="aboutUsWrapper">
         <div className="aboutUsBanner">
           <div className="aboutUsBannerContent">
             <h1 className="aboutUsBannerHeading">About Us</h1>
-            <span>&#8595;</span>
+            {/* <span>&#8595;</span> */}
           </div>
         </div>
         <div className="aboutUsText">
-            <h1 className="aboutUsTextHeading">Our Firm</h1>
-            <p className="aboutUsTextContent">At D2A Atelier our goal is very simple &#8220;It&#39;s your space&#8221;. Our talented and attentive team love to listen your valuable needs or suggestions which help us to complete your dream home. Your dreams, needs and goals guide our ideas and solutions from the initial concepts to the ribbon cutting. We get to know you and establish a comfortable working relationship - fostering mutual respect and open an honest communication, while our professionals bring a wealth of architectural and interior experience ideas about what is right for you. From new construction to renovation and remodeling, our experience is vast and varied.</p>
+            <h1 className="aboutUsTextHeading">Our History</h1>
+            <p className="aboutUsTextContent">D2A Atelier was founded in 2020 by its Founder & CEO Dharmender Jaishwal and Co-Founder & Managing Director Rajesh Kumar. The company has enough experience and creative minds to achive the goal and give a path to the next generation. <br/>D2A Atelier has grown its clientele rapidly since 2020 with a high level of satisfaction in their shop drawing services through their excellent man management, communication and high quality detailing.</p>
+            <h1 className="aboutUsTextHeading">Who we are?</h1>
+            <p className="aboutUsTextContent">D2A Atelier is an established business based in New Delhi.The company hold a combined experience of 8 years in the shop drawing drafting services fulfilling the needs of clients across a diverse scope within the residential, commercial and industrial sectors. <br/>
+            We are well equipped to offer reliable. soilutions to the modern architectural millwork industry. Our business prides itself on listening to our clients&#39; needs and strives to understand their standard for shop drawings.<br/>
+            Our vision is &#34;To succeed by delivering a reliable service with a high level of professionalism&#34;. We take this very seriously and offer our list of services below to you as a potential or ongoing client.</p>
+            <h1 className="aboutUsTextHeading">Our values</h1>
+            <p className="aboutUsTextContent">
+              Our values underpin everything we do at D2A Atelier. The way we approach our work and the way we interact with our colleagues and clients. </p>  
+            <p className="aboutUsTextContent">
+              QUALITY<br/>
+              This value is critical to our company and our staff members. It is an imperative part of our ser- vice and professionalism.</p>
+            <p className="aboutUsTextContent">
+              DEPENDABILITY<br/>
+              Our client must be able to rely on us to deliver on our promise without worry with the project quality. You can rely on D2A Atelier to complete.</p>
+            <p className="aboutUsTextContent">
+              INTEGRITY<br/>
+              We foster the highest standards of personal con- duct in our people stressing the importance of courtesy, honesty, trust and understanding.</p>
+            <p className="aboutUsTextContent">
+              PRIDE<br/>
+              All member of staff at D2A Atelier quickly devel- op a sense of professional pride in their work, skills, attitudes and in their own abilities due to the quality, dependability and integrity our com- pany offers.</p>
         </div>
         <div className="aboutUsBanner aboutUsBanner1">
           <div className="aboutUsBannerContent">
-            <h1 className="aboutUsTextHeading">Our Agenda & Vision</h1>
+            <h1 className="aboutUsTextHeading">Meet Our Team</h1>
           </div>
         </div>
-        <div className="aboutUsText">
-            <p className="aboutUsTextContent">It is our agenda at D2A Atelier to provide client focused service through our responsible practice of architecture. Our tradition of dedication, professionalism and outstanding customer service is a treatment to that agenda as we strive each day for excellence in bringing our valued clients ideas to life.</p>
-            <p className="aboutUsTextContent" style={{fontWeight: 600}}>Vision Statement:</p>
-            <p className="aboutUsTextContent">To be viewed as a respected architectural firm, providing high quality design and services to our clients with honesty and integrity.</p>
-            <p className="aboutUsTextContent" style={{fontWeight: 600}}>Purpose Statement:</p>
-            <p className="aboutUsTextContent">To serve our neighbours and project partners with responsible responsive design that comes from attentive discussions and meaningful solutions to produce our clients desire.</p>
-        </div>
-        <div className="aboutUsBanner aboutUsBanner2">
-          <div className="aboutUsBannerContent">
-            <h1 className="aboutUsTextHeading">Harmony wih nature</h1>
-          </div>
-        </div>
-        <div className="aboutUsText">
-            <p className="aboutUsTextContent">&#8220;Study nature, love nature, stay close to nature. It will never fail you.&#8221; These words are said by the great architect Frank Lloyd and we are following these terms in our designs. <br/>
-            If green design is important to you we will make it a priority. D2A understands the importance of environmental stewardship in project design. House owner and designers continue to be inspired to design houses that are  more earth-friendly. Our approach often focuses on energy efficiency, as we use enhanced insulating material and install advanced heating and cooling system. We add solar panels to rooftops and build wind farms to generate electricity.
-            </p>
-        </div>
+        <Team />
     </div>
   )
 }
---
+++ Components/Team.jsx
@@ -0,0 +1,125 @@
+export default function Team() {
+    return(
+        <div className="teamWrapper">
+            <div className="teamFounder">
+                <div className="teamMemberBlock">
+                    <div className="teamFounderImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/dharmender.jpg" alt="Founder" />
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Dharmender Jaiswal</p>
+                        <a href="https://www.linkedin.com/in/dharmender-jaishwal/" target="_blank" rel="noreferrer" className="teamFounderSocial">
+                            <img className="teamMemberLinkedinIcon" src="/Assets/linkedin.svg" alt="LinkedIn" />
+                        </a>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Founder & CEO</p>
+                    </div>
+                </div>
+                <div className="teamMemberBio">
+                    <p className="teamMemberBioText">
+                    Dharmender Jaishwal experience spans across 4 all aspects of the industry including major infrastructure, residential and commercial sectors. After the fundamental and practical aspects of each sector were established, Jaishwal went on to excel in Project Management, Site Management and Operations.
+                    </p>
+                </div>
+            </div>
+            <div className="teamFounder">
+                <div className="teamMemberBlock">
+                    <div className="teamFounderImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/rajesh.jpg" alt="Founder" />
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Rajesh Kumar</p>
+                        <a href="https://www.linkedin.com/in/rajeshkr1997/" target="_blank" rel="noreferrer" className="teamFounderSocial">
+                            <img className="teamMemberLinkedinIcon" src="/Assets/linkedin.svg" alt="LinkedIn" />
+                        </a>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Co-Founder & Managing Director</p>
+                    </div>
+                </div>
+                <div className="teamMemberBio">
+                    <p className="teamMemberBioText">
+                    Rajesh Kumar has worked at the Skets Studio and HBA International, are most experienced companies in outsourcing and designing industry. He has enough knowledge and experience to manage the projects and deliver in estimated time. Kumar is using his past exeperinece to grow the company and to making generations to more knowledgeable and skillfull for future.
+                    </p>
+                </div>
+            </div>
+
+            <div className="teamMembers">
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/jasmine.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Jasmine</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Sr. BDM</p>
+                    </div>
+                </div>
+
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/rabacca.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Rabacca</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Team Leader</p>
+                    </div>
+                </div>
+
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/arvind.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Arvind</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Intermediate Architect</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="teamMembers">
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/fiyazul.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Fiyazul</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Intermediate Architect</p>
+                    </div>
+                </div>
+
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/nikhil.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Nikhil</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Junior Architect</p>
+                    </div>
+                </div>
+
+                <div className="teamMemberBlock">
+                    <div className="teamMemberImageContainer">
+                        <img className="teamMemberImage" src="/Assets/Team/rahul.jpg" alt="Founder"/>
+                    </div>
+                    <div className="teamMemberNameWrapper">
+                        <p className="teamMemberName">Rahul</p>
+                    </div>
+                    <div className="teamMemberDesignationWrapper">
+                        <p className="teamMemberDesignation">Junior Architect</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
